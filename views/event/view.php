69e2db13602bc610aeb3f8119ddecad66a8c9e2d
devcws <dev@creativa.ws>
2015-03-03 12:20:20 -0500

    tabsEvento

--- views/event/view.php
+++ views/event/view.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\bootstrap\Tabs;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Event */
@@ -12,53 +13,21 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="event-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Actualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Eliminar', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-//            'id',
-            'name',
-            'short_description:ntext',
-            'general_content:ntext',
-            'methodology:ntext',
-            'addressed_to:ntext',
-            'included:ntext',
-            'requirements:ntext',
-            'file:ntext',
-            'photo:ntext',
-            'url:ntext',
-            'begin_at',
-            'end_at',
-            'city',
-            'cost',
-            'discount',
-            'discount_end_at',
-            'discount_description',
-            'year',
-            'status',
-//            'created_at',
-//            'updated_at',
-//            'country_id',
-            [                    // the owner name of the model
-                'label' => 'Pais',
-                'value' => $model->country->name,
+    <?= Tabs::widget([
+        'items' => [
+            [
+                'label' => 'Evento',
+//                'content' => 'Anim pariatur cliche...',
+                'content' => $this->render('_partialEvent',['model'=>$model]),
+                'active' => true
             ],
-//            'eventtype_id',
-            [                    // the owner name of the model
-                'label' => 'Tipo de Evento',
-                'value' => $model->eventtype->name,
+            [
+                'label' => 'Preguntas del Evento',
+//                  'content' => 'Anim pariatur cliche...',
+                'content' => $this->render('_partialEventquestion',['dataProvider'=>$dataProvider, 'searchModel'=>$searchModel]),
+//                 'options' => ['id' => 'myveryownID'],
             ],
         ],
     ]) ?>
