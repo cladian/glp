62ef59146d437db5c11d17686fbc55be157a1ff9
edison <edison@cladian.com>
2015-03-04 20:10:12 -0500

    Grid view eventAsnwer y Answer

--- views/inscription/_partialEventanswer.php
+++ views/inscription/_partialEventanswer.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use kartik\editable\Editable;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\EventanswerSearch */
@@ -17,7 +18,84 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 
-    <?= GridView::widget([
+    <?
+    $gridColumns = [
+        // the name column configuration
+        [
+            'class' => '\kartik\grid\SerialColumn',
+            'contentOptions'=>['class'=>'kartik-sheet-style'],
+        ],
+        [
+            'class' => 'kartik\grid\EditableColumn',
+            'readonly' => true,
+            'attribute' => 'eventquestion_id',
+            'value' => function ($data) {
+                return $data->eventquestion->question->text;
+            }
+
+        ],
+
+        [
+            'class' => 'kartik\grid\EditableColumn',
+            'attribute' => 'reply',
+            /*'readonly' => function ($model, $key, $index, $widget) {
+                return (!$model->status == 10); // do not allow editing of inactive records
+            },*/
+            'editableOptions' => [
+                'header' => 'Nombre',
+                'size' => 'md',
+                'inputType' => \kartik\editable\Editable::INPUT_TEXTAREA,
+                'class' => 'text-danger',
+                'format' => Editable::FORMAT_BUTTON,
+                /*'options' => [
+                    'pluginOptions' => ['min' => 0, 'max' => 5000]
+                ]*/
+            ],
+        ],
+        [
+            'class' => '\kartik\grid\BooleanColumn',
+            'attribute' => 'status',
+            'trueLabel' => '1',
+            'falseLabel' => '0'
+        ],
+
+
+    ];
+
+    echo \kartik\grid\GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => $gridColumns,
+        // set your toolbar
+        'toolbar' => [
+
+            '{export}',
+            '{toggleData}',
+        ],
+        'bordered' => true,
+        'resizableColumns' => true,
+        'striped' => true,
+        'condensed' => true,
+        'responsive' => true,
+        'hover' => true,
+        'showPageSummary' => true,
+        'persistResize' => false,
+        'exportConfig' => true,
+        /*        'pjax'=>true,
+                'floatHeader'=>true,
+                'floatHeaderOptions'=>['scrollingTop'=>'50'],
+                'pjaxSettings'=>[
+                    'neverTimeout'=>true,
+                    'beforeGrid'=>'My fancy content before.',
+                    'afterGrid'=>'My fancy content after.',
+                ]*/
+    ]);
+
+
+
+
+
+    /*= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
@@ -38,6 +116,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 //            ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+    ]); */
+    ?>
 
 </div>
