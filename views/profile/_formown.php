149535406497b5a5ddb88d32817f17227649fa3a
DVO593 <d@cladian.com>
2015-03-11 17:01:55 -0500

    Usarios

--- views/profile/_formown.php
+++ views/profile/_formown.php
@@ -22,48 +22,63 @@ use app\models\Country;
 <div class="profile-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Crear' : 'Guardar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        <?= Html::a('Cancelar', ['/profile/viewown'], ['class' => 'btn btn-danger']) ?>
-    </div>
+    
 
-    <div class="col-sm-4">
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
         <?= $form->field($model, 'name')->textInput(['maxlength' => 100]) ?>
         <?= $form->field($model, 'lastname')->textInput(['maxlength' => 100]) ?>
-        <?= $form->field($model, 'responsability_name')->textInput(['maxlength' => 250]) ?>
-
-        <?=
-        $form->field($model, 'responsibilitytype_id')->dropDownList(
-            ArrayHelper::map(Responsibilitytype::find()->all(), 'id', 'name'),
-            ['prompt' => 'Seleccione']
-        ) ?>
-        <?= $form->field($model, 'institution_name')->textInput(['maxlength' => 250]) ?>
-        <?=
-        $form->field($model, 'institutiontype_id')->dropDownList(
-            ArrayHelper::map(Institutiontype::find()->all(), 'id', 'name'),
-            ['prompt' => 'Seleccione']
-        ) ?>
-    </div>
-    <div class="col-sm-4">
+        <?= $form->field($model, 'gender')->dropDownList(['M' => 'M', 'F' => 'F',], ['prompt' => '']) ?>
         <?= $form->field($model, 'phone_number')->textInput(['maxlength' => 15]) ?>
         <?= $form->field($model, 'mobile_number')->textInput(['maxlength' => 15]) ?>
-        <?= $form->field($model, 'gender')->dropDownList(['M' => 'M', 'F' => 'F',], ['prompt' => '']) ?>
+        
 
-        <?=
-        $form->field($model, 'country_id')->dropDownList(
+        
+        
+    </div>
+    
+
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <?=$form->field($model, 'country_id')->dropDownList(
             ArrayHelper::map(Country::find()->all(), 'id', 'name'),
             ['prompt' => 'Seleccione']
         ) ?>
+        <?= $form->field($model, 'institution_name')->textInput(['maxlength' => 250]) ?>
+        <?=$form->field($model, 'institutiontype_id')->dropDownList(
+            ArrayHelper::map(Institutiontype::find()->all(), 'id', 'name'),
+            ['prompt' => 'Seleccione']
+        ) ?>
+        
+        <?= $form->field($model, 'responsability_name')->textInput(['maxlength' => 250]) ?>
+        <?=$form->field($model, 'responsibilitytype_id')->dropDownList(
+            ArrayHelper::map(Responsibilitytype::find()->all(), 'id', 'name'),
+            ['prompt' => 'Seleccione']
+        ) ?>
+        
+        
     </div>
-    <div class="col-sm-3">
-        <?= Html::img($model->getImageUrl(),['class'=>'img-responsive img-thumbnail']);?>
+    
+    <center>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <?= Html::img($model->getImageUrl(),['class'=>'img-responsive img-thumbnail']);?>
+        </div>
+    </center>
 
-
-</div>
-
-
-
+    <center>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+            <div class="form-group">
+                <?= Html::submitButton($model->isNewRecord ? '<span class="glyphicon glyphicon-floppy-disk"></span> Crear' : '<span class="glyphicon glyphicon-floppy-disk"></span> Guardar', ['class' => $model->isNewRecord ? 'btn btn-success btn-lg btn-block' : 'btn btn-primary btn-lg btn-block']) ?>
+                
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+            <div class="form-group">
+                
+                <?= Html::a('<span class="glyphicon glyphicon-remove"></span> Cancelar', ['/profile/viewown'], ['class' => 'btn btn-danger btn-lg btn-block']) ?>
+            </div>
+        </div>
+    </center>
     <?php ActiveForm::end(); ?>
 
 </div>
