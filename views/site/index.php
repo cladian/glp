ded190a864ce0ffc50e80fee0cc77f9c3511b1ac
DVO593 <d@cladian.com>
2015-03-12 11:29:06 -0500

    Merge branch 'daniel'
    Conflicts:
    	views/site/admUser.php

--- views/site/index.php
+++ views/site/index.php
@@ -6,6 +6,81 @@ $this->title = 'My Yii Application';
 
 ?>
 
+<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+<div class="panel panel-default">
+  <div class="panel-heading">Panel heading without title</div>
+  <div class="panel-body">
+    <ul class="bs_timeline">
+        
+        <li class="timeline-right"> 
+            <div class="timeline-panel">
+                <div class="timeline-copy">
+                    <h5 class="timeline-title">Responsive Timeline (w/ Bootstrap)</h5>
+                    <p style="margin-top: 14px;"><a href="http://alexwhinfield.co.uk/responsive-css-timeline-twitter-bootstrap/" title="Grab the source code"><span class="label label-success">Built by Alex Whinfield</span></a></p>
+                </div>
+            </div>
+            <div class="timeline-badge"><i class="glyphicon glyphicon-envelope"></i></div>
+        </li>
+
+
+        <li class="timeline-right">
+            <div class="timeline-panel">
+                <div class="timeline-copy">
+                    <h5 class="timeline-title">Eiusmod Tempor Incididunt</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                </div>
+            </div>
+            <div class="timeline-badge info"><i class="glyphicon glyphicon-search"></i></div>
+        </li>
+
+
+        <li class="timeline-right">
+            <div class="timeline-panel">
+                <div class="timeline-copy">
+                    <h5 class="timeline-title">Eiusmod Tempor Incididunt</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                </div>
+            </div>
+            <div class="timeline-badge warning"><i class="glyphicon glyphicon-user"></i></div>
+        </li>
+
+        <li class="timeline-right">
+            <div class="timeline-panel">
+                <div class="timeline-copy">
+                    <h5 class="timeline-title">Eiusmod Tempor Incididunt</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                </div>
+            </div>
+            <div class="timeline-badge warning"><i class="glyphicon glyphicon-user"></i></div>
+        </li>
+
+        <li class="timeline-right">
+            <div class="timeline-panel">
+                <div class="timeline-copy">
+                    <h5 class="timeline-title">Eiusmod Tempor Incididunt</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                </div>
+            </div>
+            <div class="timeline-badge warning"><i class="glyphicon glyphicon-user"></i></div>
+        </li>
+
+        <li class="timeline-right">
+            <div class="timeline-panel">
+                <div class="timeline-copy">
+                    <h5 class="timeline-title">Eiusmod Tempor Incididunt</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                </div>
+            </div>
+            <div class="timeline-badge warning"><i class="glyphicon glyphicon-user"></i></div>
+        </li>
+
+
+    </ul>
+  </div>
+</div>
+    
+
+
 <div class="site-index">
 
     <div class="jumbotron">
