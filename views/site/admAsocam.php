889f5a729c7bd0a3f727674297c9f7447c792502
edison <edison@cladian.com>
2015-03-12 00:49:21 -0500

    las changes

--- views/site/admAsocam.php
+++ views/site/admAsocam.php
@@ -5,8 +5,8 @@ use kartik\widgets\Growl;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-$this->title = 'Panel de control de usuario';
-$this->params['breadcrumbs'][] = $this->title;
+/*$this->title = 'Bienvenido ASOCAM';
+$this->params['breadcrumbs'][] = $this->title;*/
 
 // http://demos.krajee.com/widget-details/growl
 
@@ -30,7 +30,7 @@ if (!$hasProfile){
 }
 ?>
 <div class="hidden-xs">
-    <h3><?= Html::encode($this->title) ?></h3>
+    <!--<h3><?/*= Html::encode($this->title) */?></h3>-->
     <div class="btn-group btn-group-justified" role="group" aria-label="...">
 <!--Panel-->        
         <div class="hidden-xs col-xs-6 col-lg-3 col-md-6">
@@ -42,7 +42,7 @@ if (!$hasProfile){
                         </div>
                         <div class="col-xs-9 text-right">
                             <div class="huge"><?= $activeUsers; ?></div>
-                            <div>Usuario</div>
+                            <div>Usuarios</div>
                         </div>
                     </div>
                 </div>
@@ -96,7 +96,7 @@ if (!$hasProfile){
                 </div>
                 <a href="#">
                     <div class="panel-footer">
-                        <span class="pull-left"><?= Html::a('Ver todas..',['incription/index'])?></span>
+                        <span class="pull-left"><?= Html::a('Ver todas..',['inscription/index'])?></span>
                         <span class="pull-right"><i class="glyphicon glyphicon-circle-arrow-right"></i></span>
                         <div class="clearfix"></div>
                     </div>
@@ -106,7 +106,7 @@ if (!$hasProfile){
 <!--END Panel-->
 <!--Panel-->        
         <div class="hidden-xs col-xs-6 col-lg-3 col-md-6">
-            <div class="panel panel-red">
+            <div class="panel panel-info">
                 <div class="panel-heading">
                     <div class="row">
                         <div class="col-xs-3">
@@ -175,40 +175,7 @@ if (!$hasProfile){
                     <div class="chat-panel panel panel-primary">
                         <div class="panel-heading">
                             <i class="fa fa-comments fa-fw"></i>
-                            Notificaciones
-                            <div class="btn-group pull-right">
-                         <!--       <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-chevron-down"></i>
-                                </button>-->
-                                <!--<ul class="dropdown-menu slidedown">
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-refresh fa-fw"></i> Refresh
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-check-circle fa-fw"></i> Available
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-times fa-fw"></i> Busy
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-clock-o fa-fw"></i> Away
-                                        </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-sign-out fa-fw"></i> Sign Out
-                                        </a>
-                                    </li>
-                                </ul>-->
-                            </div>
+                            Solicitudes por atender
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
@@ -245,17 +212,131 @@ if (!$hasProfile){
                         <!-- /.panel-body -->
                         <div class="panel-footer">
                             <div class="input-group">
-                                <!--<input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                                <button class="btn btn-warning btn-md" id="btn-chat">
+                                    Ver todas
+                                </button>
+                               <!-- <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
                                 <span class="input-group-btn">
-                                    <button class="btn btn-warning btn-sm" id="btn-chat">
-                                        Send
-                                    </button>
+
                                 </span>-->
                             </div>
                         </div>
                         <!-- /.panel-footer -->
                     </div>
                     <!-- /.panel .chat-panel -->            
+
+</div>
+<div class="col-xs-12 col-lg-4 col-md-4 col-lg-4">
+
+        <!-- /.panel -->
+                    <div class="chat-panel panel panel-primary">
+                        <div class="panel-heading">
+                            <i class="fa fa-comments fa-fw"></i>
+                            Solicitudes por atender
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <ul class="chat">
+                                <?php foreach($modelRequest as $request){?>
+                                    <li class="left clearfix">
+                                    <span class="chat-img pull-left">
+                                      <?= Html::img($request->inscription->user->profiles->getImageUrl(), ['class' => 'img-circle','style'=>'height:50px;']); ?>
+                                    </span>
+                                        <div class="chat-body clearfix">
+                                            <div class="header">
+                                                <strong class="primary-font"><?= $request->inscription->user->username?></strong>
+                                                <small class="pull-right text-muted">
+                                                    <i class="fa fa-clock-o fa-fw"></i> <?= Yii::$app->formatter->asDate($request->created_at, 'long'); ?>
+                                                </small>
+                                            </div>
+                                            <p>
+                                                <?= $request->question; ?>
+
+                                            </p>
+
+
+                                            <?= Html::a('Responder', ['reply/create'], ['class' => 'btn btn-primary btn-xs pull-right']) ?>
+                                        </div>
+                                    </li>
+                                <?php
+
+                                }
+                                ?>
+
+
+                            </ul>
+                        </div>
+                        <!-- /.panel-body -->
+                        <div class="panel-footer">
+                            <div class="input-group">
+                                <button class="btn btn-warning btn-md" id="btn-chat">
+                                    Ver todas
+                                </button>
+                               <!-- <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                                <span class="input-group-btn">
+
+                                </span>-->
+                            </div>
+                        </div>
+                        <!-- /.panel-footer -->
+                    </div>
+                    <!-- /.panel .chat-panel -->
+
+</div><div class="col-xs-12 col-lg-4 col-md-4 col-lg-4">
+
+        <!-- /.panel -->
+                    <div class="chat-panel panel panel-primary">
+                        <div class="panel-heading">
+                            <i class="fa fa-comments fa-fw"></i>
+                            Solicitudes por atender
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <ul class="chat">
+                                <?php foreach($modelRequest as $request){?>
+                                    <li class="left clearfix">
+                                    <span class="chat-img pull-left">
+                                      <?= Html::img($request->inscription->user->profiles->getImageUrl(), ['class' => 'img-circle','style'=>'height:50px;']); ?>
+                                    </span>
+                                        <div class="chat-body clearfix">
+                                            <div class="header">
+                                                <strong class="primary-font"><?= $request->inscription->user->username?></strong>
+                                                <small class="pull-right text-muted">
+                                                    <i class="fa fa-clock-o fa-fw"></i> <?= Yii::$app->formatter->asDate($request->created_at, 'long'); ?>
+                                                </small>
+                                            </div>
+                                            <p>
+                                                <?= $request->question; ?>
+
+                                            </p>
+
+
+                                            <?= Html::a('Responder', ['reply/create'], ['class' => 'btn btn-primary btn-xs pull-right']) ?>
+                                        </div>
+                                    </li>
+                                <?php
+
+                                }
+                                ?>
+
+
+                            </ul>
+                        </div>
+                        <!-- /.panel-body -->
+                        <div class="panel-footer">
+                            <div class="input-group">
+                                <button class="btn btn-warning btn-md" id="btn-chat">
+                                    Ver todas
+                                </button>
+                               <!-- <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                                <span class="input-group-btn">
+
+                                </span>-->
+                            </div>
+                        </div>
+                        <!-- /.panel-footer -->
+                    </div>
+                    <!-- /.panel .chat-panel -->
 
 </div>
 
