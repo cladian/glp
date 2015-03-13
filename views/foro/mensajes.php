278958ea07f979b37b6f538227d5a56cb9b60a3b
DVO593 <d@cladian.com>
2015-03-13 10:54:29 -0500

    Foro

--- views/foro/mensajes.php
+++ views/foro/mensajes.php
@@ -40,24 +40,95 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-6">
-          <div class="input-group">
-            <span class="input-group-addon">
-              <input type="radio" aria-label="...">
-            </span>
-            <input type="text" class="form-control" aria-label="...">
-          </div><!-- /input-group -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" value="">
+              Nombre Completo
+            </label>
+          </div>
         </div><!-- /.col-lg-6 -->
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" value="">
+              Nombre Completo
+            </label>
+          </div>
+        </div><!-- /.col-lg-6 -->
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" value="">
+              Nombre Completo
+            </label>
+          </div>
+        </div><!-- /.col-lg-6 -->
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" value="">
+              Nombre Completo
+            </label>
+          </div>
+        </div><!-- /.col-lg-6 -->
+        </div>
       </div><!-- /.row -->
     </div>
   </div>
+
+
+
+
   <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
     <div class="panel panel-yellow">
       <div class="panel-heading">Mensajes recibidos</div>
       <div class="panel-body">
-        Panel content
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <p class="bg-info">Enviado por mcrettaz</p> 
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <p class="bg-warning">11 de noviembre de 2014</p>
+        </div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quasi molestiae voluptatibus iusto optio consequatur eveniet quidem sunt placeat. A error magni voluptate ea nesciunt cupiditate natus fuga, quo esse.
+        <hr>
+         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <p class="bg-info">Enviado por mcrettaz</p> 
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <p class="bg-warning">11 de noviembre de 2014</p>
+        </div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quasi molestiae voluptatibus iusto optio consequatur eveniet quidem sunt placeat. A error magni voluptate ea nesciunt cupiditate natus fuga, quo esse.
+        <hr>
+         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <p class="bg-info">Enviado por mcrettaz</p> 
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <p class="bg-warning">11 de noviembre de 2014</p>
+        </div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quasi molestiae voluptatibus iusto optio consequatur eveniet quidem sunt placeat. A error magni voluptate ea nesciunt cupiditate natus fuga, quo esse.
       </div>
     </div>
+    <nav>
+  <ul class="pagination">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
   </div>
 
 </div>
