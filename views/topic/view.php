e7676fad3fdab53e167e512bfe238517a1018297
devcws <dev@creativa.ws>
2015-03-18 09:51:01 -0500

    boton

--- views/topic/view.php
+++ views/topic/view.php
@@ -17,14 +17,17 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Actualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Eliminar', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
             ],
         ]) ?>
+        <?= Html::a('Subir Documento', ['phforum/createdoc', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Subir Video', ['phforum/createvideo', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Subir Imagen', ['phforum/createimg', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
     </p>
 
     <?= DetailView::widget([
