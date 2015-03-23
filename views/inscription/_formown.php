42346826003b2b01b380f03dea880e324ef9c540
devcws <dev@creativa.ws>
2015-03-23 12:14:31 -0500

    view

--- views/inscription/_formown.php
+++ views/inscription/_formown.php
@@ -10,7 +10,8 @@ use app\models\User;
 use app\models\Event;
 use kartik\widgets\DepDrop;
 use yii\helpers\Url;
-
+use kartik\widgets\DatePicker;
+use kartik\widgets\TimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Inscription */
@@ -101,17 +102,147 @@ use yii\helpers\Url;
                 'offText' => 'NO',
             ]
         ]); ?>
-    </div>
 
+    </div>
+    <div class="panel panel-primary">
+        <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+        <div class="panel-body">
+
+
+            <!---->
+
+            <?= $form->field($modelLogistic, 'leavingonorigincity')->textInput(['maxlength' => 45]) ?>
+            <?=  $form->field($modelLogistic, 'residence')->widget(SwitchInput::classname(), [
+                'pluginOptions' => [
+                    'onText' => 'SI',
+                    'offText' => 'NO',
+                ]
+            ]);?>
+
+
+            <?= $form->field($modelLogistic, 'residenceobs')->textarea(['rows' => 6]) ?>
+
+            <div class="panel panel-success">
+                <div class="panel-heading"><h5>Información de llegada</h5></div>
+                <div class="panel-body">
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'leavingonairline')->textInput(['maxlength' => 45]) ?>
+                    </div>
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'leavingonflightnumber')->textInput(['maxlength' => 10]) ?>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'leavingondate')->widget(DatePicker::classname(), [
+                            'options' => ['placeholder' => 'Fecha'],
+                            'pluginOptions' => [
+                                'autoclose' => true,
+                                'format' => 'yyyy-mm-dd',
+                                'readonly' => true,
+                                'disabled' => true,
+                            ]
+                        ]);
+                        ?>
+
+                    </div>
+                    <div class="col-sm-3 col-xs-6">
+
+                        <?= $form->field($modelLogistic, 'leavingonhour')->widget(TimePicker::classname(), [
+                            'pluginOptions' => [
+                                'showSeconds' => false,
+                                'showMeridian' => false,
+                            ]
+                        ]); ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="panel panel-info">
+                <div class="panel-heading"><h5>Información de Salida</h5></div>
+                <div class="panel-body">
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'returningonairline')->textInput(['maxlength' => 45]) ?>
+                    </div>
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'returningonflightnumber')->textInput(['maxlength' => 10]) ?>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'returningondate')->widget(DatePicker::classname(), [
+                            'options' => ['placeholder' => 'Fecha'],
+                            'pluginOptions' => [
+                                'autoclose' => true,
+                                'format' => 'yyyy-mm-dd',
+
+                                'readonly' => true,
+                                'disabled' => true,
+                            ]
+                        ]);
+                        ?>
+
+                    </div>
+                    <div class="col-sm-3 col-xs-6">
+
+
+                        <?= $form->field($modelLogistic, 'returningonhour')->widget(TimePicker::classname(), [
+                            'pluginOptions' => [
+                                'showSeconds' => false,
+                                'showMeridian' => false,
+                            ]
+                        ]); ?>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="panel panel-warning">
+                <div class="panel-heading"><h5>Información de Alojamiento</h5></div>
+                <div class="panel-body">
+
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'accommodationdatein')->widget(DatePicker::classname(), [
+                            'options' => ['placeholder' => 'Fecha'],
+                            'pluginOptions' => [
+                                'autoclose' => true,
+                                'format' => 'yyyy-mm-dd',
+
+                                'readonly' => true,
+                                'disabled' => true,
+                            ]
+                        ]);
+                        ?>
+
+                    </div>
+                    <div class="col-sm-3 col-xs-6">
+                        <?= $form->field($modelLogistic, 'accommodationdateout')->widget(DatePicker::classname(), [
+                            'options' => ['placeholder' => 'Fecha'],
+                            'pluginOptions' => [
+                                'autoclose' => true,
+                                'format' => 'yyyy-mm-dd',
+
+                                'readonly' => true,
+                                'disabled' => true,
+                            ]
+                        ]);
+                        ?>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!---->
+
+    </div>
 
 
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    
+
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Crear' : 'Guardar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-</div>        
+</div>
 </div>
     <?php ActiveForm::end(); ?>
 
