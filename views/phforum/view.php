4266f33c708fe77fb2096fa88329d7c8559384b7
devcws <dev@creativa.ws>
2015-03-26 14:57:53 -0500

    foro

--- views/phforum/view.php
+++ views/phforum/view.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\grid\GridView;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Phforum */
@@ -55,12 +57,134 @@ $this->title = $model->name;
                 'method' => 'post',
             ],
         ]) ?>
-        <?= Html::a(\Yii::$app->params['btnTema'], ['topic/create', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(\Yii::$app->params['btnSubirD'], ['phforum/createdoc', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(\Yii::$app->params['btnSubirV'], ['phforum/createvideo', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(\Yii::$app->params['btnSubirI'], ['phforum/createimg', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+        <?/*= Html::a(\Yii::$app->params['btnTema'], ['topic/create', 'id' => $model->id ], ['class' => 'btn btn-primary']) */?><!--
+        <?/*= Html::a(\Yii::$app->params['btnSubirD'], ['phforum/createdoc', 'id' => $model->id ], ['class' => 'btn btn-primary']) */?>
+        <?/*= Html::a(\Yii::$app->params['btnSubirV'], ['phforum/createvideo', 'id' => $model->id ], ['class' => 'btn btn-primary']) */?>
+        --><?/*= Html::a(\Yii::$app->params['btnSubirI'], ['phforum/createimg', 'id' => $model->id ], ['class' => 'btn btn-primary']) */?>
     </p>
 
     
 
 </div>
+
+<!--INICIOVista Temas-->
+
+<div class="panel panel-green">
+    <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+    <div class="panel-body">
+        <?= GridView::widget([
+            'dataProvider' => $dataProviderTopic,
+            'filterModel' => $searchTopic,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+//            'id',
+                'content:ntext',
+                'status',
+                'created_at',
+                'updated_at',
+                // 'user_id',
+                // 'phforum_id',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+    </div>
+</div>
+
+
+    <p>
+
+        <?= Html::a(\Yii::$app->params['btnTema'], ['topic/create', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+
+<!--FIN Vista Temas-->
+
+<!--INICIOVista Documentos-->
+
+    <<div class="panel panel-green">
+    <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+    <div class="panel-body">
+        <?= GridView::widget([
+            'dataProvider' => $dataProviderPDocument,
+            'filterModel' => $searchPDocument,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'phforum_id',
+                'document_id',
+                'created_at',
+                'updated_at',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+
+    </div>
+</div>
+
+
+        <p>
+
+            <?= Html::a(\Yii::$app->params['btnSubirD'], ['phforum/createdoc', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+
+<!--FIN Vista Documentos-->
+
+<!--INICIOVista Videos-->
+
+<div class="panel panel-green">
+    <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+    <div class="panel-body">
+        <?= GridView::widget([
+            'dataProvider' => $dataProviderPVideo,
+            'filterModel' => $searchPVideo,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'phforum_id',
+                'video_id',
+                'created_at',
+                'updated_at',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+    </div>
+</div>
+
+
+            <p>
+
+                <?= Html::a(\Yii::$app->params['btnSubirV'], ['phforum/createvideo', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+
+
+<!--FIN Vista Videos-->
+
+<!--INICIOVista Imagenes-->
+
+<div class="panel panel-green">
+    <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+    <div class="panel-body">
+
+        <?= GridView::widget([
+            'dataProvider' => $dataProviderPImagen,
+            'filterModel' => $searchPImagen,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'phforum_id',
+                'imagen_id',
+                'created_at',
+                'updated_at',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+    </div>
+</div>
+
+
+                <p>
+
+
+                    <?= Html::a(\Yii::$app->params['btnSubirI'], ['phforum/createimg', 'id' => $model->id ], ['class' => 'btn btn-primary']) ?>
+<!--FIN Vista Imagenes-->
