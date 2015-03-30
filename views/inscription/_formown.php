4d7ee0b1a86a47687c02384c6656d358ce978e64
devcws <dev@creativa.ws>
2015-03-30 10:20:31 -0500

    botones

--- views/inscription/_formown.php
+++ views/inscription/_formown.php
@@ -86,21 +86,33 @@ use kartik\widgets\TimePicker;
 
     </div>
     <div class="col-sm-6">
-        <?= $form->field($model, 'exposition')->widget(SwitchInput::classname(), [
-            'pluginOptions' => [
-                'onText' => 'SI',
-                'offText' => 'NO',
-            ]
-        ]); ?>
+
+        <!--          --><?/*= $form->field($model, 'exposition')->widget(SwitchInput::classname(), [
+              'pluginOptions' => [
+                  'onText' => 'SI',
+                  'offText' => 'NO',
+              ]
+          ]); */?>
+
+        <?=
+        $form->field($model, 'exposition')->dropDownList(['0' => 'NO', '1' => 'SI']);
+        ?>
+
     </div>
 
     <div class="col-sm-6">
-        <?= $form->field($model, 'service_terms')->widget(SwitchInput::classname(), [
-            'pluginOptions' => [
-                'onText' => 'SI',
-                'offText' => 'NO',
-            ]
-        ]); ?>
+
+        <?/*= $form->field($model, 'service_terms')->widget(SwitchInput::classname(), [
+              'pluginOptions' => [
+                  'onText' => 'SI',
+                  'offText' => 'NO',
+              ]
+          ]); */?>
+
+        <?=
+        $form->field($model, 'service_terms')->dropDownList(['0' => 'NO', '1' => 'SI']);
+        ?>
+
     </div>
 </div>
 </div>
@@ -114,14 +126,16 @@ use kartik\widgets\TimePicker;
 
             <?= $form->field($modelLogistic, 'leavingonorigincity')->textInput(['maxlength' => 45]); ?>
 
-            <?=  $form->field($modelLogistic, 'residence')->widget(SwitchInput::classname(), [
-                'pluginOptions' => [
-                    'onText' => 'SI',
-                    'offText' => 'NO',
+            <!--        --><?/*=  $form->field($modelLogistic, 'residence')->widget(SwitchInput::classname(), [
+            'pluginOptions' => [
+                'onText' => 'SI',
+                'offText' => 'NO',
+            ]
+        ]);*/?>
 
-                ]
-            ]);?>
-
+            <?=
+            $form->field($modelLogistic, 'residence')->dropDownList(['0' => 'NO', '1' => 'SI']);
+            ?>
 
             <?= $form->field($modelLogistic, 'residenceobs')->textarea(['rows' => 6]) ?>
 
