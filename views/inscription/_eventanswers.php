179139cb8e4c4562c1c4d1292a651d01cc95b4ac
DVO593 <d@cladian.com>
2015-03-31 09:48:12 -0500

    Regreso de cambios

--- views/inscription/_eventanswers.php
+++ views/inscription/_eventanswers.php
@@ -9,48 +9,17 @@ use kartik\builder\Form;
 /* @var $this yii\web\View */
 /* @var $model app\models\Eventanswer */
 
-$this->title = 'Actualizar Respuesta por Evento: ';
+$this->title = 'Actualizar Respuesta por Evento: ' ;
 //$this->params['breadcrumbs'][] = ['label' => 'Respuestas por Evento', 'url' => ['index']];
 //$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 //$this->params['breadcrumbs'][] = 'Actualziación';
 ?>
 
 <div class="regresar">
-    <?= Html::a(\Yii::$app->params['btnRegresar'], ['/inscription/viewown', 'id' => $id], ['class' => 'btn btn-default']) ?>
+<?= Html::a(\Yii::$app->params['btnRegresar'],['/inscription/viewown','id' => $id], ['class' => 'btn btn-default'])?>
 </div>
 
-<div class="panel panel-primary">
-<<<<<<< HEAD
-    <div class="panel-heading">Preguntas del Evento</div>
-    <div class="panel-body">
-        <?php
-        $form = ActiveForm::begin();?>
-
-        <?= TabularForm::widget([
-            'dataProvider' => $dataProvider,
-            'form' => $form,
-            'attributes' => $model->getGrid(),
-            /*    'attributeDefaults' => [
-
-                    [
-                        'type' => Form::INPUT_RAW,
-                        'value'=>function ($model, $key, $index, $widget) {return $model->eventquestion->text; }
-
-
-                    ],
-                ],*/
-            'actionColumn' => false,
-            'serialColumn' => false,
-            'checkboxColumn' => false,
-        ]);
-        // Add other fields if needed or render your submit button
-        ?>
-
-    </div>
-    <div class="panel-footer">
-        <?= Html::submitButton(\Yii::$app->params['btnGuardarSiguiente'], ['class' => 'btn btn-primary']); ?>
-        </div>
-=======
+<div class="panel panel-green">
   <div class="panel-heading">Preguntas del Evento</div>
   <div class="panel-body">
     <?php
@@ -79,10 +48,11 @@ $form = ActiveForm::begin();?>
 ]);
 // Add other fields if needed or render your submit button
 ?>
-<?=Html::submitButton(\Yii::$app->params['btnGuardar'], ['class' => 'btn btn-primary']);?>
+<?=Html::submitButton(\Yii::$app->params['btnGuardar'], ['class' => 'btn btn-success']);?>
   </div>
->>>>>>> daniel
 </div>
+
+
 
 
 <?php ActiveForm::end(); ?>
