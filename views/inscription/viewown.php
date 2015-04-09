48e8be4f3155430e97e1153ef58fd8db5ce85160
edison <edison@cladian.com>
2015-04-09 07:44:10 -0500

    nueva version

--- views/inscription/viewown.php
+++ views/inscription/viewown.php
@@ -11,6 +11,9 @@ use yii\bootstrap\Modal;
 $this->title = $model->id;
 /*$this->params['breadcrumbs'][] = ['label' => 'Inscripciones', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;*/
+$clase='green';
+if ($model->complete <100)
+$clase='red';
 ?>
 
 <div class="breadcrumb">
@@ -26,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;*/
     <?php
     Modal::begin([
         'header' => '<h4>Inscripción</h3>',
-        'toggleButton' => ['label' => \Yii::$app->params['btnHelp'],'class'=>'btn btn-default pull-right'],
+        'toggleButton' => ['label' => \Yii::$app->params['btnHelp'], 'class' => 'btn btn-default pull-right'],
     ]);
 
     echo $this->render('/help/inscription-index');
@@ -48,9 +51,83 @@ foreach (Yii::$app->session->getAllFlashes() as $key => $message) {
              </div>';
 }
 ?>
+<div class="btn-group btn-group-justified" role="group" aria-label="...">
+
+
+    <!--Panel-->
+    <div class=" col-xs-6 col-lg-3 col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i style="font-size:2em;" class="glyphicon glyphicon-question-sign"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?= $model->complete_logistic; ?>%</div>
+                        Información Logistica
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!--Panel-->
+    <div class=" col-xs-6 col-lg-3 col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i style="font-size:2em;" class="glyphicon glyphicon-question-sign"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?= $model->complete_eventquiz; ?>%</div>
+                        Preguntas por evento
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class=" col-xs-6 col-lg-3 col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i style="font-size:2em;" class="glyphicon glyphicon-question-sign"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?= $model->complete_quiz; ?>%</div>
+                        Preguntas generales
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!--END Panel-->
+    <!--Panel-->
+    <div class=" col-xs-6 col-lg-3 col-md-3">
+        <div class="panel panel-<?= $clase?>">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i style="font-size:2em;" class="glyphicon glyphicon-question-sign"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?= $model->complete; ?>%</div>
+                        Total
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-    <!--Visualización información del evento-->
+
 
     <div class="panel panel-primary">
         <?= $this->render('/event/_detailinfo', ['model' => $model->event]) ?>
@@ -59,7 +136,7 @@ foreach (Yii::$app->session->getAllFlashes() as $key => $message) {
             <!--  <?= Html::a(\Yii::$app->params['btnRegresar'], ['/site/index'], ['class' => 'btn btn-default']) ?> -->
                 <span class="pull-right">
 
-               
+
 
                 </span>
         </div>
@@ -73,103 +150,57 @@ foreach (Yii::$app->session->getAllFlashes() as $key => $message) {
 <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
 
 
-    <div class="panel-group" id="accordion" role="tablist">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h5 class="panel-title">
 
-        <div class="panel panel-primary">
-            <div class="panel-heading" role="tab" id="headingOne">
-                <h5 class="panel-title">
+                <span class="glyphicon glyphicon-file" aria-hidden="true"></span>
+                Información de Inscripción <span class="pull-right"><?= $model->complete; ?>%
 
-                    <span class="glyphicon glyphicon-file" aria-hidden="true"></span>
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
-                       aria-controls="collapseOne">
-                        Información de Inscripción <span class="pull-right"><?= $model->complete; ?>%</span>
-                    </a>
-                </h5>
-            </div>
-            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                <div class="panel-body">
-                    <?= $this->render('_partialInscription', ['model' => $model, 'modelProfile' => $modelProfile]) ?>
-                </div>
 
-            </div>
+            </h5>
         </div>
 
-        <div class="panel panel-primary">
-            <div class="panel-heading" role="tab" id="headingTwo" aria-multiselectable="true">
-                <h4 class="panel-title">
-                    <span class="glyphicon glyphicon-plane" aria-hidden="true"></span>
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"
-                       aria-expanded="true" aria-controls="collapseTwo">
-                        Información logística
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                <div class="panel-body">
-                    <?= $this->render('_partialLogistic', ['model' => $modelLogistic]) ?>
+        <div class="panel-body">
+            <?= $this->render('_partialInscription', ['model' => $model, 'modelProfile' => $modelProfile]) ?>
 
-                </div>
 
-            </div>
         </div>
+        <div class="panel-heading" >
+        <h4 class="panel-title">
+            <span class="glyphicon glyphicon-plane" aria-hidden="true"></span>
 
+                Información logística
 
-        <!--        <div class="panel panel-primary">
-            <div class="panel-heading" role="tab" id="headingThree">
-                <h4 class="panel-title">
-                    <span class="glyphicon glyphicon-list" aria-hidden="true"></span>
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree"
-                       aria-expanded="false" aria-controls="collapseThree">
-                        Respuestas por evento
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                <div class="panel-body">
-                    <? /*= $this->render('_partialEventanswerown', ['searchModel' => $searchModelEventanswer, 'dataProvider' => $dataProviderEventanswer]); */ ?>
-                </div>
-            </div>
-        </div>-->
-
-
-        <!--        <div class="panel panel-primary">
-            <div class="panel-heading" role="tab" id="heading4">
-                <h4 class="panel-title">
-                    <span class="glyphicon glyphicon-list" aria-hidden="true"></span>
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse4"
-                       aria-expanded="false" aria-controls="collapse4">
-                        Respuestas a preguntas generales
-                    </a>
-                </h4>
-            </div>
-            <div id="collapse4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading4">
-                <div class="panel-body">
-                    <? /*= $this->render('_partialAnswerown', ['searchModel' => $searchModelAnswer, 'dataProvider' => $dataProviderAnswer]); */ ?>
-                </div>
-            </div>
-        </div>-->
-
-
-        <div class="panel panel-success">
-            <div class="panel-heading" role="tab" id="heading5">
-                <h4 class="panel-title">
-                    <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse5"
-                       aria-expanded="false" aria-controls="collapse5">
-                        Notificaciones / Solicitudes
-                    </a>
-                </h4>
-            </div>
-            <div id="collapse5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading5">
-                <div class="panel-body">
-                    <?= $this->render('_partialRequest', ['searchModel' => $searchModelRequest, 'dataProvider' => $dataProviderRequest]); ?>
-                    <?= Html::a(\Yii::$app->params['btnEnviar'], ['request/createown', 'inscription_id' => $model->id], ['class' => 'btn btn-success']) ?>
-                </div>
-            </div>
+        </h4>
+    </div>
+        <div class="panel-body">
+            <?= $this->render('_partialLogistic', ['model' => $modelLogistic]) ?>
         </div>
-
 
     </div>
+
+
+
+
+    <div class="panel panel-success">
+        <div class="panel-heading" role="tab" id="heading5">
+            <h4 class="panel-title">
+                <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+
+                   Inquietudes
+
+            </h4>
+        </div>
+
+            <div class="panel-body">
+                <?= $this->render('_partialRequest', ['searchModel' => $searchModelRequest, 'dataProvider' => $dataProviderRequest]); ?>
+                <?= Html::a(\Yii::$app->params['btnEnviar'], ['request/createown', 'inscription_id' => $model->id], ['class' => 'btn btn-success']) ?>
+            </div>
+
+    </div>
+
+
 </div>
 
 <!--ENDRegistro-->
