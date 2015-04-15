32d6b6c577764e4a6171ea62e6ef86e7cbe56686
devcws <dev@creativa.ws>
2015-04-15 10:11:45 -0500

    ayuda

--- views/help/inscription-index.php
+++ views/help/inscription-index.php
@@ -15,7 +15,7 @@
 <p><h4>Estructura</h4>
 <ul type = circle>
     <li>Pasos para actualizar información
-    <li>Sección Informativa y de avance de actualiazcón
+    <li>Sección Informativa y de avance de actualización de información
     <li>Sección Inquietudes y Respuestas
     <li>Completar Registro
     <li>Nueva Inquietud
@@ -25,10 +25,15 @@
 
 <hr/>
 
-<h2><span style="color: #808080;">Pasos para actualizar información</span></h2>
-<p><strong>Paso 1.- </strong><td>Dar clic en <strong>Sección 1.Información General</strong> y se mostrara toda la información general y logística por actualizar</td>
-<p><strong>Paso 2.- </strong><td>Dar clic en <strong>Sección 1.Información General</strong> y se mostrara toda la información general y logística por actualizar</td>
-<p><strong>Paso 3.- </strong><td>Dar clic en <strong>Sección 1.Información General</strong> y se mostrara toda la información general y logística por actualizar</td>
+<!--<h2><span style="color: #808080;">Pasos para actualizar información</span></h2>-->
+<h2><span style="color: #4f83b8;">Pasos para actualizar información</span></h2>
+<p><strong>Paso 1.- </strong><td>Damos clic en <strong>Sección 1.Información General</strong> y se mostrara toda la información general y logística por actualizar</td>
+<p><strong>Paso 2.- </strong><td>Damos clic en <strong>Sección 2. Experiencia y expectativa</strong> y se mostraran las preguntas Espesificas del Evento </td>
+<p><strong>Paso 3.- </strong><td>Damos clic en <strong>Sección 3. Información logística.</strong> y se mostraran preguntas en general del Evento </td>
 
-<h2><span style="color: #808080;">Pasos para actualizar información</span></h2>
+<hr/>
+
+<h2><span style="color: #4f83b8;">Sección Informativa y de avance en la  actualización de información</span></h2>
 </p>
+
+
